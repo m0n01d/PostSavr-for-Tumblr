1ec3eac4a91e4e6e53a59a3d0ed65db53f9f1de6
JobIIn <dwightdoane@DwightsMac.local>
2014-07-01 11:27:48 -0400

    Upload v1
    First upload of v1 of PostSavr

--- README.md
+++ README.md
@@ -2,3 +2,37 @@ PostSavr-for-Tumblr
 ===================
 
 A tumblr extension to save posts for later
+
+Requires localForage and jQuery
+
+when you hover over the Heart on a post, a text box pops up
+
+you enter the tags you want, and you can either hit ‘enter’ or click on the heart to save that post
+
+Tags are added to a section under Radar.
+
+Clicking on a tag loads those posts back to the top of your dash
+
+Posts load 10 at a time
+
+Click on 'LOAD MORE SAVED POSTS' to see more posts
+
+When you’re done you can click on ‘Clear Saved Posts’ and your dash will go back to normal
+
+You can remove individual posts from tags, or remove a tag itself
+
+All data is stored in the browser
+
+feedback:
+  email : dwightdoane@me.com
+
+
+How to install on Chrome:
+  Download the extension file from the website and save it to your computer.
+  Click the Chrome menu icon Chrome menu on the browser toolbar.
+  Select Tools > Extensions.
+  Locate the extension file on your computer and drag the postSavr.CRX file onto the Extensions page.
+  Review the list of permissions in the dialog that appears. If you would like to proceed, click Install.
+
+How to install on Safari:
+  Double click ‘postSavr.safariextz’
---
+++ main.js
@@ -0,0 +1,290 @@
+(function() {
+var app = {};
+  function injectCSS() {
+    var headTag = document.getElementsByTagName("head")[0].innerHTML;
+    var newCSS = headTag + '<style type="text/css">'+
+    '.post_full .post_control.ps_remove:after {background-position: -914px -123px}'+
+    '.none{display:none;}'+
+    'span.ps_delete_tag{color: rgba(255, 0, 0, .5) !important;float:right; cursor: pointer;}'+
+    'a.ps_tag{display:inline !important; padding: 0 !important;}'+
+    'span.tag_length{float: right; margin: 0 7px;}'+
+    'div.tag_wrap{display: block;position: relative;padding: 2px 5px 0 20px;line-height: 36px;margin: 0;color: rgba(255,255,255,.50);font-size: 14px;font-weight: bold;text-decoration: none;border-top: 0;}'+
+    '#clearDash{cursor: pointer;}'+
+    '#saveDone {display: none;color: rgba(255, 255, 255, 0.498039);position: fixed;bottom: 12px;left: 35px;z-index: 10000;}'+
+    '#load_more {border-radius: 5px; cursor: pointer; background: white; padding: 10px; text-align: center;}'+
+    '</style>';
+    document.getElementsByTagName('head')[0].innerHTML += newCSS;
+  }
+
+  injectCSS();
+
+  $('.right_column').append('<div id="post_Savr_container"><ul class="ps_list controls_section"><li class="section_header account_header">PostSavr</li><li class="section_header account_header none" id="clearDash">Clear saved posts</li></ul></div>');
+  localforage.getItem('postSavr_tags', function(postSavr_tags) {
+    var list = '';
+    $.each(postSavr_tags, function(key, value) {
+      var tag = "<li class='ps_list_item'>" +
+                  "<div class='tag_wrap' href='#'>"+
+                    "<div class='hide_overflow'>"+
+                    " <a class='ps_tag'>" + key + "</a>"+
+                    "<span  data-tag="+key+" class='ps_delete_tag'> X</span>"+
+                    "<span data-length_for='" + key.replace(/\s/g, '_') + "' class='tag_length'> " + postSavr_tags[key].post_ids.length + "</span>"+
+                    "</div>"+
+                  "</div>"+
+                "</li>";
+      list += tag;
+    });
+    $('#clearDash').before(list);
+  });
+
+  $('body').on('mouseover', '.like', function() {
+    var $this = $(this),
+      pos = $this.position(),
+      x = pos.left - 100,
+      y = pos.top - 25,
+      input = '<input type="text" placeholder="Comma sperated tags" style=" display: none; position: absolute; top: ' + y + 'px; left: ' + x + 'px; " class="tagInput" name="inputValue" size="20" >';
+    if ($('.tagInput').length <= 0) {
+      $this.closest('.post').append(input);
+      $('.tagInput').fadeIn('fast', function() {
+        $(this).focus();
+      });
+    }
+  });
+
+  $('body').on('click', '.like', function() {
+    var $this = $(this),
+    post = $this.closest('.post'),
+    inputValue = $('.tagInput').val();
+    //$this.addClass('liked');
+    $('.tagInput').remove();
+    $('.post_animated_heart').remove();
+
+    post.find('.post_controls_inner').find('.ps_remove').remove();
+    post.find('.post_controls_inner').append('<a href="#" class="post_control ps_remove" title="Remove post from tag"></a>');
+    post.find('.post_controls_inner').find('.xkit-interface-control-button').remove();
+    submitValues(post, inputValue);
+    post.find('.post_controls_inner').find('.ps_remove').remove();
+
+
+  });
+
+  $('body').on('mouseleave', '.post', function() {
+    $('.tagInput').fadeOut('fast', function() {
+      this.remove();
+    });
+  });
+
+  $('body').on('keyup', '.tagInput', function(e) {
+    if (e.keyCode == 13) {
+      var post = $(this).closest('.post'),
+      inputValue = $('.tagInput').val();
+      $('.tagInput').remove();
+
+      if(inputValue === '') return;
+       post.find('.post_controls_inner').find('.ps_remove').remove();
+      post.find('.post_controls_inner').append('<a href="#" class="post_control ps_remove" title="Remove post from tag"></a>');
+      post.find('.post_controls_inner').find('.xkit-interface-control-button').remove();
+      submitValues(post, inputValue);
+      post.find('.post_controls_inner').find('.ps_remove').remove();
+    }
+  });
+
+
+  $('body').on('click', '.ps_tag', function(e) {
+    e.preventDefault();
+    $('[data-postSavr~=true]').remove();
+
+    var $posts = $('#posts'),
+    index = 0;
+    tag = $(this).text();
+    app.tag = tag;
+    app.index = 0;
+    $('.highlight').removeClass('highlight');
+    $(this).addClass('highlight');
+    $('#clearDash').removeClass('none');
+
+    renderPosts(tag)
+
+  });
+
+  $('body').on('click', '#load_more', function() {
+    renderPosts(app.tag);
+  });
+
+  function renderPosts(tag) {
+
+    var load_more = '<li class="post_container" id="load_more"><div>LOAD MORE SAVED POSTS</div></li>'
+    var $posts = $('#posts');
+    localforage.getItem('postSavr_tags', function(postSavr_tags) {
+      var arr = postSavr_tags[tag].root_ids;
+      if(arr.length > 10) {
+        if(arr.length > 10 && $('#load_more').length <= 0) {
+          if ($('#new_post_buttons').length > 0) {
+            $('#new_post_buttons').after(load_more);
+          }
+          else {
+           $posts.prepend(load_more);
+          }
+        }
+        var index = app.index % arr.length || 0;
+        // save next index - for next call
+        app.index += 10;
+        $.map(arr.slice(index, index + 10), function(root_id) {
+          localforage.getItem('p_s'+root_id, function(html) {
+            $('#load_more').before($(html));
+            var count = $('[data-postSavr~=true]').length;
+            if(count >= arr.length ){
+              $('#load_more').remove();
+              return;
+            }
+          });
+        });
+      }
+      else {
+        postSavr_tags[tag].root_ids.reverse().forEach(function(root_id) {
+        localforage.getItem('p_s'+root_id, function(html) {
+          if ($('#new_post_buttons').length > 0) {
+            $('#new_post_buttons').after($(html));
+          }
+          else {
+           $posts.prepend($(html));
+          }
+        });
+      });
+      }
+    })
+  }
+
+  $('body').on('click', '.ps_remove', function(e) {
+    e.preventDefault();
+    var post = $(this).closest('.post'),
+    root_id = post.data('root-id');
+
+    localforage.getItem('postSavr_tags', function(postSavr_tags) {
+
+      var tag = postSavr_tags[app.tag],
+       index = tag.root_ids.indexOf(root_id);
+
+      tag.root_ids.splice(index, 1);
+      tag.blog_names.splice(index, 1);
+      tag.post_ids.splice(index, 1);
+
+      localforage.removeItem('p_s'+root_id);
+
+      if(tag.root_ids.length ===0) {
+        delete postSavr_tags[app.tag];
+      }
+
+      localforage.setItem('postSavr_tags', postSavr_tags, function() {
+        post.remove();
+        updateCount(app.tag, -1);
+      });
+
+    });
+  });
+
+  $('body').on('click', '.ps_delete_tag', function() {
+    var parent = $(this).closest('.ps_list_item');
+    parent.remove();
+
+    var tag = $(this).data('tag');
+    localforage.getItem('postSavr_tags', function(postSavr_tags) {
+      var _tag = postSavr_tags[tag];
+      _tag.root_ids.forEach(function(root_id) {
+        localforage.removeItem('p_s'+root_id);
+      });
+      delete postSavr_tags[tag];
+      localforage.setItem('postSavr_tags', postSavr_tags, function() {
+        parent.remove();
+      });
+    });
+  });
+
+
+  $('body').on('click', '#clearDash', function() {
+    $(this).addClass('none');
+    $('[data-postSavr~=true]').remove();
+    $('.highlight').removeClass('highlight');
+    $('.selected').removeClass('selected')
+  });
+
+
+  function submitValues(post, inputValue) {
+    console.log(inputValue);
+    if(inputValue === '') return;
+
+
+
+    var tags = inputValue.split(','),
+    root_id = post.data('root-id'),
+    post_id = post.data('post-id'),
+    blog_name = post.data('tumblelog-name'),
+    html = post.closest('.post')[0].outerHTML;
+    $('.tagInput').remove();
+    tagPosts(tags, root_id, post_id, blog_name);
+    saveHtml(root_id, html);
+  }
+
+  function tagPosts(tags, root_id, post_id, blog_name) {
+    localforage.getItem('postSavr_tags', function(e) {
+      var postSavr_tags = e || {};
+      tags.forEach(function(tag) {
+        tag = tag.trim();
+        if (postSavr_tags[tag] === undefined) {
+          var tag_item = "<li class='ps_list_item'>" +
+                  "<div class='tag_wrap' href='#'>"+
+                    "<div class='hide_overflow'>"+
+                    " <a class='ps_tag'>" + tag + "</a>"+
+                    "<span  data-tag="+tag+" class='ps_delete_tag'> X</span>"+
+                    "<span data-length_for='" + tag.replace(/\s/g, '_')  + "' class='tag_length'>1</span>"+
+                    "</div>"+
+                  "</div>"+
+                "</li>";
+          $('#clearDash').before(tag_item);
+          postSavr_tags[tag] = {
+            post_ids: [post_id],
+            blog_names: [blog_name],
+            root_ids: [root_id]
+          };
+        }
+        else {
+          if (postSavr_tags[tag].root_ids.indexOf(root_id) >= 0) {
+          } else {
+            updateCount(tag, 1);
+            postSavr_tags[tag].blog_names.unshift(blog_name);
+            postSavr_tags[tag].post_ids.unshift(post_id);
+            postSavr_tags[tag].root_ids.unshift(root_id);
+          }
+        }
+      });
+      localforage.setItem('postSavr_tags', postSavr_tags, function() {
+        $('#container').append('<div id="saveDone" class="">Saved to: ' + tags+'</div>');
+        $('#saveDone').fadeIn(250, function() {
+          var self = $(this);
+            $(this).delay(500).fadeOut(250, function() {
+              self.remove();
+          });
+        });
+      });
+    });
+  }
+
+  function saveHtml(root_id, html) {
+    html = '<li class="post_container" data-postSavr="true">' + html + '</li>';
+    localforage.setItem('p_s'+root_id, html, function(e) {});
+  }
+
+  function updateCount(tag, incrementor) {
+    tag = tag.replace(/\s/g, '_');
+    var newval = parseInt($('[data-length_for~=' + tag + ']').text(), 10) + incrementor;
+    if (newval < 1) {
+      $('[data-length_for~=' + tag + ']').closest('.ps_list_item').remove();
+    }
+    $('[data-length_for~=' + tag + ']').text(newval);
+  }
+
+
+})();
+
+
+
+
